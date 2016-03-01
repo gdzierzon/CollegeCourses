f080ca8b127220f1cf5ba893537fb629b8944032
gdzierzon <gdzierzon@gmail.com>
2016-03-01 00:22:14 -0700

    Add acceptance test feature. Will implement last after CollegeCourse and College objects are fully tested/

---
+++ main/CollegeSolution/CollegeUnitTests/AcceptanceTests/AddCourses.feature
@@ -0,0 +1,16 @@
+﻿Feature: AddCourses
+	In order to add courses
+	As a university administrator
+	I want to be shown a sequential list of courses
+
+Scenario: Add Courses to College
+	Given I have added values to the college
+	| values                                         |
+	| Course A,Course B: Course A                    |
+	| Course A,Course B: Course A,Course C: Course B |
+	
+	When I press add
+	Then the result courses should be displayed
+	| courses                      |
+	| Course A, Course B           |
+	| Course A, Course B, Course C |
---
+++ main/CollegeSolution/CollegeUnitTests/AcceptanceTests/AddCoursesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CollegeUnitTests.AcceptanceTests
+{
+    [Binding]
+    public class AddCoursesSteps
+    {
+        [Given(@"I have added values to the college")]
+        public void GivenIHaveAddedValuesToTheCollege(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result courses should be displayed")]
+        public void ThenTheResultCoursesShouldBeDisplayed(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
